404a1220d89143a77c64f0195f76a6d83ed8a992
1104611 <1104611@1104611NB01.iead.local>
2016-04-07 13:36:22 +0800

    Create Code

---
+++ fileupload_jsf/src/main/java/com/iisi/core/utils/EnumOperationCode.java
@@ -0,0 +1,40 @@
+package com.iisi.core.utils;
+
+public enum EnumOperationCode {
+	
+	LoginLogQueryServiceImpl("LQ"), 
+	OperationLogServiceImpl("OQ"),
+	AddUserServiceImpl("NU"),
+	FileDeleteServiceImpl("FD"),
+	FileQueryServiceImpl("FQ"),
+	FileUploadServiceImpl("FU"),
+	QueryUserServiceImpl("AQ"),
+	UpdatePwdServiceImpl("PM");
+	
+	
+	private String value;
+	
+	EnumOperationCode(String value){
+		this.value = value;		
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	public static EnumOperationCode getCodeName(String operationName){
+		EnumOperationCode rtnCode = null;
+		for(EnumOperationCode code : EnumOperationCode.values()){
+			if(operationName.equals(code.toString())){
+				System.out.println("code value = " + code.toString());
+				rtnCode = code;
+			}
+		}
+		return rtnCode;
+	}
+	
+}
